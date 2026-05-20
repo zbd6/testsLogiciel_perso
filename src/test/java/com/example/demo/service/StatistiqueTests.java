c23f17951ca7707995ec9260e0de9a6f1f39a514
zbd6 <bibzoubida6@gmail.com>
2026-05-20 16:15:36 +0200

    test stat

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -5,13 +5,40 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
-
 
 @SpringBootTest
 public class StatistiqueTests {
 
     @MockBean
     StatistiqueImpl statistiqueImpl;
+
+    @Test
+    void testPrixMoyenAvecUneVoiture() {
+        StatistiqueImpl statistique = new StatistiqueImpl();
+        Voiture voiture = new Voiture("Peugeot", 10000);
+
+        statistique.ajouter(voiture);
+        Echantillon echantillon = statistique.prixMoyen();
+
+        assertEquals(1, echantillon.getNombreDeVoitures());
+        assertEquals(10000, echantillon.getPrixMoyen());
+    }
+
+    @Test
+    void testPrixMoyenAvecPlusieursVoitures() {
+        StatistiqueImpl statistique = new StatistiqueImpl();
+
+        statistique.ajouter(new Voiture("Peugeot", 10000));
+        statistique.ajouter(new Voiture("Renault", 20000));
+        statistique.ajouter(new Voiture("BMW", 30000));
+
+        Echantillon echantillon = statistique.prixMoyen();
+
+        assertEquals(3, echantillon.getNombreDeVoitures());
+        assertEquals(20000, echantillon.getPrixMoyen());
+    }
 
 }
