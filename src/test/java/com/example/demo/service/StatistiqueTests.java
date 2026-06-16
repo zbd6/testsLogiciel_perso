8800b6b7590322588c87300a41befff445d436e8
zbd6 <bibzoubida6@gmail.com>
2026-06-16 12:05:04 +0200

    Ajout test cas liste vide + @BeforeEach dans StatistiqueTests

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.data.Voiture;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -15,12 +16,17 @@ public class StatistiqueTests {
     @MockBean
     StatistiqueImpl statistiqueImpl;
 
+    StatistiqueImpl statistique;
+
+    @BeforeEach
+    void init() {
+        statistique = new StatistiqueImpl();   // un objet tout neuf avant chaque test
+    }
+
     @Test
     void testPrixMoyenAvecUneVoiture() {
-        StatistiqueImpl statistique = new StatistiqueImpl();
-        Voiture voiture = new Voiture("Peugeot", 10000);
+        statistique.ajouter(new Voiture("Peugeot", 10000));
 
-        statistique.ajouter(voiture);
         Echantillon echantillon = statistique.prixMoyen();
 
         assertEquals(1, echantillon.getNombreDeVoitures());
@@ -29,8 +35,6 @@ public class StatistiqueTests {
 
     @Test
     void testPrixMoyenAvecPlusieursVoitures() {
-        StatistiqueImpl statistique = new StatistiqueImpl();
-
         statistique.ajouter(new Voiture("Peugeot", 10000));
         statistique.ajouter(new Voiture("Renault", 20000));
         statistique.ajouter(new Voiture("BMW", 30000));
@@ -39,6 +43,12 @@ public class StatistiqueTests {
 
         assertEquals(3, echantillon.getNombreDeVoitures());
         assertEquals(20000, echantillon.getPrixMoyen());
+    }
+
+    @Test
+    void testPrixMoyenSansVoiture() {
+        // aucune voiture ajoutee -> division par zero -> ArithmeticException
+        assertThrows(ArithmeticException.class, () -> statistique.prixMoyen());
     }
 
 }
